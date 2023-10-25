793b8f9302fafa666567bddfaef62da4ad99f1e0
brd3v <bnsiq2015@gmail.com>
2023-10-25 20:43:17 -0300

    exercicio ts

---
+++ exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+let mutiplica = (multiplicando, multiplicador) => multiplicando * multiplicador;
+console.log(mutiplica(10, 5));
+let saldação = (nome) => `Olá ${nome}`;
+console.log(saldação('Bruno'));
---
+++ exercicio.ts
@@ -0,0 +1,6 @@
+let mutiplica = (multiplicando: number, multiplicador: number) : number => multiplicando * multiplicador
+
+console.log(mutiplica(10,5))
+
+let saldação = (nome:string): string => `Olá ${nome}`
+console.log(saldação('Bruno'))
